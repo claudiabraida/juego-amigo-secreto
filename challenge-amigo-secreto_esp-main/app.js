23d80e8680d8c730ffacbf635cb66d0eda21480e
claudiabraida <braidacreaciones@gamail.com>
2025-01-29 14:02:12 -0300

    funcionalidad agregar nombres en html y array, validacion

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Capturar el valor del campo de entrada: Utilizar document.getElementById o document.querySelector para obtener el texto ingresado por el usuario.
+//evento botón "button-add" HTML
+// let $inputAmigo;
+const $ = element => document.querySelector(element);
+
+
+
+let arrayAmigos = []
+function agregarAmigo () {
+  $inputAmigo = $("#amigo") 
+  if($inputAmigo.value === "") {
+    alert("Por favor, inserte un nombre.")
+  } 
+  
+  $ulListaAmigos = $("#listaAmigos").innerHTML += `<li>${$inputAmigo.value}</li>`
+  arrayAmigos.push($inputAmigo.value)
+  
+  limpiarCampoDeEntrada()
+  console.log(arrayAmigos)
+}
+
+function limpiarCampoDeEntrada () {
+  $inputAmigo.value = ""
+}
+
+// function sortearAmigo() { 
+//   if () {
+      
+    
+// }
+// }
+
