01e60962359549b5013604309e7bab1a67060020
claudiabraida <braidacreaciones@gamail.com>
2025-02-05 23:20:00 -0300

    modificaciones  y cambios de archivos

---
+++ app.js
@@ -0,0 +1,51 @@
+/* 💥💥💥💥💥💥 VARIABLES 💥💥💥💥💥💥 */
+const $ = element => document.querySelector(element);
+$ulListaAmigos = $("#listaAmigos")
+$inputAmigo = $("#amigo")
+let arrayAmigos = []
+
+
+/* 💥💥💥💥💥💥 FUNCION PARA ELEMENTO HTML 💥💥💥💥💥💥 */
+function elementoVacio (elemento) {
+  elemento.value = ""
+}
+
+
+/* 💥💥💥💥💥💥 FUNCION MANEJO DOM 💥💥💥💥💥💥 */
+function agregarAmigo () {
+  const nombreIngresado = $inputAmigo.value.trim().replace(/\s+/g, " ")
+  $ulListaAmigos.innerHTML += `<li>${nombreIngresado}</li>`
+  elementoVacio($inputAmigo)
+  
+  if(nombreIngresado === "") {
+    alert("Por favor, inserte un nombre.")
+  } else {
+    actualizarArray (nombreIngresado)
+  }  
+}
+
+/* 💥💥💥💥💥💥 FUNCION ACTUALIZAR DATOS (ARRAY) 💥💥💥💥💥💥 */
+function actualizarArray (nombreIngresado) {
+  arrayAmigos.push(nombreIngresado)
+  console.log(arrayAmigos)
+}
+
+
+/* 💥💥💥💥💥💥 FUNCION SORTEAR AMIGOS 💥💥💥💥💥💥 */
+function sortearAmigo() {
+  if (arrayAmigos.length > 1) {
+    let indiceAleatorioAmigo = Math.floor(Math.random()*arrayAmigos.length);
+    let amigosorteado = arrayAmigos[indiceAleatorioAmigo]
+    console.log(amigosorteado)
+    
+    mostrarAmigoElegido(amigosorteado)
+  } if (arrayAmigos.length === 1) {
+    alert("necesitas ingresar al menos 2 nombres para sortear ")
+  }
+}
+
+/* 💥💥💥💥💥💥 FUNCION MOSTRAR AMIGOS 💥💥💥💥💥💥 */
+function mostrarAmigoElegido(amigosorteado) {
+  $ulListaAmigos.innerHTML = ""
+  $ulResultado = $("#resultado").innerHTML = `<li>El amigo secreto es:${amigosorteado}</li>`
+}
